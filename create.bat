df39847382d2cca26da6b10467d9222e359edb7e
unknown <dk107dk@hotmail.com>
2025-08-06 12:32:22 -0700

    tweak

--- create.bat
+++ create.bat
@@ -1,7 +1,21 @@
 
-copy_msixes.bat
-create_windows_universal.bat
-create_windows_universal_test.bat
-sign_windows_msixes.bat
-sign_windows_universal_bundle.bat
+echo.
+echo ************ copying ************ 
+call copy_msixes.bat
+
+echo.
+echo ************ signing ************
+call sign_windows_msixes.bat
+
+echo.
+echo ************ creating prod universal ************
+call create_windows_universal.bat
+
+echo.
+echo ************ creating test universal ************
+call create_windows_universal_test.bat
+
+echo.
+echo ************ signing test universal ************
+call sign_windows_universal_bundle.bat
 
--- sign_windows_universal_bundle.bat
+++ sign_windows_universal_bundle.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 
 echo ========================================
-echo Signing FlightPath ARM64 + Universal
+echo Signing FlightPath Test Universal
 echo ========================================
 
 REM Configuration
@@ -10,7 +10,7 @@ set APP_NAME=FlightPathData
 set BASE_DIR=C:\dev\flightpath_packaging\universal
 set DIST_DIR=%BASE_DIR%\dist
 set VERSION = "1.1.15"
-set BUNDLE_NAME=%APP_NAME%-Universal-1.1.15-test.msixbundle
+set BUNDLE_NAME=%APP_NAME%-Universal-test.msixbundle
 
 
 

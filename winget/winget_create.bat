3312969644ec3584a1ce518f14a5d0b6e2bc978e
unknown <dk107dk@hotmail.com>
2025-09-12 05:59:30 -0700

    working winget

--- winget/winget_create.bat
+++ winget/winget_create.bat
@@ -2,4 +2,7 @@
 
 :: pulls a release msix to create the winget package
 :: the msix must be signed
-wingetcreate new https://github.com/dk107dk/flightpath_server/releases/download/v0.0.1-alpha/FlightPathServer-arm64-v0.0.1.msix
+
+    wingetcreate.exe new https://github.com/dk107dk/flightpath_server/releases/download/v0.0.1-alpha/FlightPathServer-arm64-test.msix
+
+
